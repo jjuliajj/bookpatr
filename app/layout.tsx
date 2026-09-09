c033dd353ab4b880e978cdfeaf6d8f2d76a7b198
tenkaihome <tenkaihome@gmail.com>
2026-09-09 16:46:05 +0700

    feat: add Whop tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,6 +116,11 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="3JFpYTJYyekSzxi08IGfboOvGItI6WtJcXnpSMZpcFU" />
         <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_gv0Hji5yYVQVHu");whop.track("page");`,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
